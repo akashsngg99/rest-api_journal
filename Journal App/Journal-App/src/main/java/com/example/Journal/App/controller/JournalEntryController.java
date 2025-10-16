1a83965d8cbfd4f805143003dc8fe4b2904fa18b
AKASH SINGH <akash.sngg99@gmail.com>
2025-10-16 22:44:26 +0530

    updated the api examples

--- Journal App/Journal-App/src/main/java/com/example/Journal/App/controller/JournalEntryController.java
+++ Journal App/Journal-App/src/main/java/com/example/Journal/App/controller/JournalEntryController.java
@@ -1,9 +1,49 @@
 package com.example.Journal.App.controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.Journal.App.entity.journalEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+
 
 @RestController
+@RequestMapping("/journal")
 public class JournalEntryController {
 
+    private Map<Long, journalEntity> journalEntries = new HashMap<>();
+
+    @GetMapping
+    public List<journalEntity> getAllEntries() {
+        // Logic to retrieve all journal entries
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody journalEntity entry) {
+        // Logic to create a new journal entry
+        journalEntries.put(entry.getId(), entry);
+
+        return true;
+    }
+
+    @GetMapping("id/{myId}")
+    public journalEntity getEntryById(@PathVariable Long myId) {
+        // Logic to retrieve a journal entry by ID
+        return journalEntries.get(myId);
+    }
+
+    @DeleteMapping("id/{myId}")
+    public journalEntity deleteEntryById(@PathVariable Long myId) {
+        // Logic to retrieve a journal entry by ID
+        return journalEntries.remove(myId);
+    }
+
+    @PatchMapping("id/{myId}")
+    public journalEntity updateEntryById(@PathVariable Long myId, @RequestBody journalEntity updatedEntry) {
+        // Logic to update a journal entry by ID
+
+        return journalEntries.put(myId, updatedEntry);
+    }
 }
